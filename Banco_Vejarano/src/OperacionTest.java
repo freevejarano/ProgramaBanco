1b148d71d44b5a8f1dd5cf0d38dc4612a995f328
alejandro <luis.vejarano01@correo.usa.edu.co>
2020-08-24 21:50:03 -0500

    Inicio pruebas unitarias

---
+++ Banco_Vejarano/src/OperacionTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperacionTest {
+
+    @org.junit.jupiter.api.Test
+    void consignarEnCuenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void retirarDinero() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void transferirACuenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void comprarConTarjeta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarCupoTCredito() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarGastoTCredito() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pagarTarjetaCredito() {
+    }
+}
--- Banco_Vejarano/src/main.java
+++ Banco_Vejarano/src/main.java
@@ -11,9 +11,8 @@ public class main {
         Cliente cli= new Cliente(1000547,"Pedro Perez");
         Cuenta cuenta= new Cuenta(cli,4785,"CuentaAhorros","Activa",15000);
         Tarjeta credito= new Tarjeta(cli,7841,"TarjetaCredito","Activa",45000,80000);
-        Prestamo prestamo= new Prestamo(cli,2123,"CuotaFija","Activa",35000)
+        Prestamo prestamo= new Prestamo(cli,2123,"CuotaFija","Activa",35000);
 
-                
     }
 
 
